02eb9e776212861553b5845d349192bf8fde23b8
lynnetteanne <lynnette.anne.linville@gmail.com>
2023-06-15 12:48:36 -0700

    guidedpractice

--- guidedpractice.js
+++ guidedpractice.js
@@ -16,12 +16,11 @@ function sumOfGifts (price1, price2) {
     return price1 + price2;
 }
 
-
 //Invoke the sumOfGifts function with the price1 and price2 variables. Store the return of this function in a new variable labeled giftTotal
 
 const giftTotal = sumOfGifts(price1 + price2);
 // giftTotal();
-console.log(giftTotal);
+// console.log(giftTotal);
 
 //Define a function that returns the cost of the giftTotal with tax (8 percent).
 
@@ -31,12 +30,17 @@ function addTax (giftTotal) {
 
 //Invoke the addTax function by passing in the giftTotal variable. Store the return of this function in a new variable labeled giftsWithTax.
 
+const giftsWithTax = addTax(giftTotal);
 
 //Define a function that compares giftsWithTax and the budget variable. The function should return true if the budget is greater than or equal to giftsWithTax.
 
+function holidayShopping(budget, giftsWithTax) {
+    return budget >= giftsWithTax;
+}
 
 //Invoke the holidayShopping function by passing in the budget and giftsWithTax variables. 
 
+holidayShopping(budget, giftsWithTax);
 
 //Optional: Try building the same solution but with the arrow syntax. 
 
